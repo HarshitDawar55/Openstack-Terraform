2cb32869fa4bf72264398a2d4abea42def02e9c5
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-07-02 19:32:07 +0530

    Started creating the provider!

--- Instance_Creation_Terraform/Instance_Creation.tf
+++ Instance_Creation_Terraform/Instance_Creation.tf
@@ -1,3 +1,8 @@
-provider "openstack" {
+# Note: While creating any instance in Openstack, the hostname of the host system and the name
+# in the hypervisor-list column of nova hypervisor-list command should match!
 
+provider "openstack" {
+  user_name = "admin"
+  tenant_name = "admin"
+  password = ""
 }
